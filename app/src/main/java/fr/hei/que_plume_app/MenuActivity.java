da78fe9c8582b0ccf048b3b654395f04cfef8f9f
Quentin Callens <quentin.callens@hei.yncrea.fr>
2021-03-26 14:29:04 +0100

    Add Main A

--- app/src/main/java/fr/hei/que_plume_app/MenuActivity.java
+++ app/src/main/java/fr/hei/que_plume_app/MenuActivity.java
@@ -25,7 +25,7 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_que);
-        mTextViewBoiteNoir = (TextView) findViewById(R.id);
+        mTextViewBoiteNoir = (TextView) findViewById(R.id.textview_boite_noir);
 
 
     }
